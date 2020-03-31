a9ea4f800997f948680c043c30c0dac12c84422a
Sobal Ivan <57543655+Anonimprogramer@users.noreply.github.com>
2020-03-31 21:56:33 +0300

    Теперь и со Слау по Гауссу
    Но только если там есть определённое решение(позже будет и с неопределёнными)

--- Matica_Class.py
+++ Matica_Class.py
@@ -2,14 +2,48 @@
 from typing import Any
 
 
+def gcd(my_list):
+    result = my_list[0]
+    for x in my_list[1:]:
+        if result < x:
+            temp = result
+            result = x
+            x = temp
+        while x != 0:
+            temp = x
+            x = result % x
+            result = temp
+    return result
+
+
+def fab(n):
+    if n < 0:
+        return -n
+    else:
+        return n
+
+
+def nok(list):
+    nod = fab(gcd(list))
+    d = nod
+    for i in list:
+        d = d * i // nod
+    return d
+
+
 class Martica(object):
 
     def __init__(self):
         self.basic = [[]]
         self.column = 0
         self.lines = 0
+        self.simple_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                               53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
         self.determine_dictionary = {}
         self.determinant = 0
+        self.transformer = [[]]
+        self.extended = []
+        self.trans = [[]]
         self.correct_protect = "incorrect"
         print("Введите количество строчек основной матрицы")
         self.lines = int(input())
@@ -30,10 +64,11 @@ class Martica(object):
                 print("Введите новое значение элемента")
                 f = int(input())
                 self.basic[i - 1][j - 1] = f
+        self.transformer = self.basic
 
     def determination(self, minor):
         if self.column != self.lines:
-            print("Матрица не является квадратичной.Вычисление определителя невозможно")
+            print("Матрица не является квадратной.Вычисление определителя невозможно")
         elif len(minor) == 2:
             e = 0
             return minor[0][0] * minor[1][1] - minor[1][0] * minor[0][1]
@@ -50,14 +85,69 @@ class Martica(object):
                 e = e + minor[0][i]*self.determination(smaller) * pow(-1, i-1)
             return e
 
+    def row_swap(self, row1, row2):
+        for i in range(len(self.transformer[0])):
+            self.transformer[row1][i], self.transformer[row2][i] = self.transformer[row2][i], self.transformer[row1][i]
 
-moy = Martica()
-ybivca =moy.determination(moy.basic)
-print("Вот он определитель", ybivca)
+    def column_swap(self,column1,column2):
+        for i in range(len(self.transformer)):
+            self.transformer[i][column1],self.transformer[i][column2] = self.transformer[i][column2],self.transformer[i][column1]
+
+    def row_transformation(self, x, row1, row2):
+
+        for i in range(len(self.transformer[0])):
+            self.transformer[row2][i] = self.transformer[row2][i] + x * self.transformer[row1][i]
+            if 0 > self.transformer[row2][i] > -0.00000000000001:
+                self.transformer[row2][i] = 0
+
+
+    def transpose(self):
+
+        """Returns the transpose of matrix A.
+        Preconditions: A is a matrix in the form of nested list."""
+
+        m = len(self.transformer)
+        n = len(self.transformer[0])
+
+        self.trans = []
+        for i in range(n):
+            self.trans.append([])
+
+        for i in range(n):
+            for j in range(m):
+                self.trans[i].append(self.transformer[j][i])
+
+        return self.trans
+
+    def rank_on_rank(self):
+        m = len(self.transformer)
+        n = len(self.transformer[0])
+        for q in range(n-1):
+            for w in range(q, m):
+                for r in self.transformer:
+                    print(r)
+                print("раздел")
+                if self.transformer[w][q] != 0:
+                    for i in range(m):
+                        if i != w and self.transformer[i][q] != 0:
+                            x = self.transformer[i][q] / self.transformer[w][q]
+                            self.row_transformation(-1 * x, w, i)
+        return self.transformer
 
 
 
-
-
-
+my = Martica()
+rank = my.rank_on_rank()
+print(rank)
+r = 0
+for i in rank:
+    r = r+1
+    first = 0
+    second = 0
+    for j in i:
+        if j !=0 and first == 0:
+            first = j
+        elif j != 0 and first != 0:
+            second = j
+    print(r,'-й корень уравнения равен',int(second/first))
 
